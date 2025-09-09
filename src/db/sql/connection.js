c2de34dc51adea5571f2bf43e8f29de5b2a2e1ce
Nick-Goof <nickgovaarts@gmail.com>
2025-09-09 11:30:01 +0200

    .env

--- src/db/sql/connection.js
+++ src/db/sql/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
-
+const dotenv = require("dotenv");
+dotenv.config();
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "NickGoof1!",
-  database: process.env.DB_DATABASE || "sakila",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   multipleStatements: true,
   waitForConnections: true,
   connectionLimit: 10,
